9a8dfc566e49f591a4aaa1a64a8d9f687ec06f43
Sarah Mustafa <you@example.com>
2024-11-11 19:43:09 +0200

    first commit

---
+++ assignmentOne/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace assignmentOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Islam's Carpet Cleaning Service\n    Charges:\n   " +
+                "     $25 per small\n        $35 per large\n " +
+                "   Sales tax rate is 6% \n   " +
+                " Estimates are valid for 30 days\n");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Enter the number of small carpet:");
+            int numberOfSmallCarpet = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number of large carpet:");
+            int numberOfLargeCarpet = int.Parse(Console.ReadLine());
+            int primaryCost = (numberOfLargeCarpet * 35 + numberOfSmallCarpet * 25);
+            double tax = primaryCost * 0.06;
+            double totalCost = tax + primaryCost;
+            Console.WriteLine($"The total cost = {totalCost:C}");
+            Console.WriteLine("this estimate is vaild for 30 days.");
+
+
+        }
+    }
+}
